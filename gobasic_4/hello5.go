8c3447a05e95407d98dd5bf72216066b63052910
joshuaparker <joshuanparker@gmail.com>
2020-06-07 23:59:21 -0400

    My first go for loop

--- gobasic_4/hello5.go
+++ gobasic_4/hello5.go
@@ -5,4 +5,8 @@ import "fmt"
 func main() {
 	fmt.Print("Hello again Go lang !")
 	fmt.println("Playing with the print and println")
+
+	for i := 0; i < 100; i++ {
+		print("Learning the go speak.")
+	}
 }
