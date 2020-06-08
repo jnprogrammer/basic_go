b1f2ccdf1b76f86d711d2dc937eecd8352da55ea
joshuaparker <joshuanparker@gmail.com>
2020-06-08 11:17:12 -0400

    Messing with the code, figured out why Go didn't want to run, All Go code must have a main function.

--- gobasic_1/hello3.go
+++ gobasic_1/hello3.go
@@ -1,12 +1,16 @@
-package hello2
+package main
 
 import "testing"
 
-func TestHello(t *testing.T) {
+func TestHello(t string) {
 	want := "Hello, World."
 	if got := Hello(); got != want {
 		t.Errorf("Hello() = %q, want $q", got, want)
 	}
+}
+
+func main() {
+	TestHello("test")
 }
 
 //5/07/2020 Come back to this and fix it.
