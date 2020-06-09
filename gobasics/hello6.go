8c9b8d8564b783fc2c55471dc2888270da4345ad
joshuaparker <joshuanparker@gmail.com>
2020-06-09 16:37:36 -0400

    Catching the return values of the Println() function

--- gobasics/hello6.go
+++ gobasics/hello6.go
@@ -3,10 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Fires of Liberation")
+
+	n, err := fmt.Println("Fires of Liberation")
+	fmt.Println(n)
+	fmt.Println(err)
 	foo()
+	bar()
 }
 
 func foo() {
 	fmt.Println("I'm not foo")
+}
+
+func bar() {
+	fmt.Println("THis is the end of the program")
 }
