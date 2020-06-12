d880d8aa3268b61b82ad561a7b6597e035a099aa
joshuaparker <joshuanparker@gmail.com>
2020-06-11 20:56:44 -0400

    Added another custom TYPE and used z to hold the conversion of x into a int.

--- challenge3.go
+++ challenge3.go
@@ -4,15 +4,24 @@ import (
 	"fmt"
 )
 
+//  := short declaration operator creates a new variable
 type grinder int //making own type with a underlying type of int
+type bong int
 
 var x grinder
+var y bong
+var z int
 
 func main() {
 	fmt.Printf("%T\n", x)
 	fmt.Println(x)
 	x = 710
 	fmt.Println(x)
+
+	z = int(x)
+	fmt.Printf("%T", y)
+	fmt.Println(y)
+
 }
 
 //Made my own type with an underlying type of int, then printed the type of the var with the
