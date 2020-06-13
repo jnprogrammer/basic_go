5ded9b80cf3185ea86f3bf9554bbf6724b154499
jnprogrammer <joshuanparker@gmail.com>
2020-06-13 16:25:10 -0400

    Seeing the difference in uint verse int, float32 to float64, complex64 to complex128 and byte and rune

--- numarictypes.go
+++ numarictypes.go
@@ -4,9 +4,26 @@ import (
 	"fmt"
 )
 
+// ints that go from -128 to 128
+
+var i8 int8
+
+//var i16 int16
+//var i32 int32
+//var i64 int64
+
+//unsigned ints 0 to 255
+//var ui8 uint8
+//var ui16 uint16
+//var ui32 uint32
+//var ui64 uint64
+
+var floats float64
+
 func main() {
-	x := 42
-	y := 42.34534
+	i8 = 42
+	floats = 42.34534
+
 	fmt.Println(x)
 	fmt.Println(y)
 	fmt.Printf("%T\n", x)
