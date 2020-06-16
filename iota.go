34a0dfb9598c1910836adec741b9b2fd02083f13
joshuaparker <joshuanparker@gmail.com>
2020-06-15 22:25:05 -0400

    learned about iota in Go, it can quickly increment a series of vars for you.

---
+++ iota.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+)
+
+//const(
+//	a = iota + 3124
+//	b = iota
+//	c = iota
+//)
+
+// TODO: iotoa will increment all var in a const and reset when the next const is declared.
+
+//const(
+//	a = iota
+//	b
+//	c
+//)
+//
+//
+//const(
+//	e = iota
+//	f
+//	g
+//)
+
+const (
+	a = iota
+	b
+	c
+	e = iota
+	f
+	g
+)
+
+func main() {
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(e)
+	fmt.Println(f)
+	fmt.Println(g)
+	fmt.Printf("%T\n", a)
+	fmt.Printf("%T\n", b)
+	fmt.Printf("%T\n", c)
+
+}
