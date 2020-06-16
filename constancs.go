b41dc4c85e0a9d23f43dff2c35885bed54c9a7cd
joshuaparker <joshuanparker@gmail.com>
2020-06-15 22:17:38 -0400

    learned constants of a kind (untyped) keep a value and allow the compliler flexablity to use it for something else.

---
+++ constancs.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+const a int = 420
+
+const (
+	b = 710
+	c = "James Bond"
+)
+
+func main() {
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Printf("%T\n", a)
+	fmt.Printf("%T\n", b)
+	fmt.Printf("%T\n", c)
+
+}
