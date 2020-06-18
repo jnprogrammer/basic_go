8217249b02ba5f821e739317a47f9e85de774362
joshuaparker <joshuanparker@gmail.com>
2020-06-17 22:21:34 -0400

    completed a challenge to print binary, decimal and hex and used bitshifting to view the change in the int variable.

---
+++ challenge7.go
@@ -0,0 +1,25 @@
+//Assign an int to a variable
+//Print that int in decimal, binary, and hex
+//shift the bits of the int over 1 and assign it to the variable
+//print that variable again in decimal, binary and hex
+
+package main
+
+import (
+	"fmt"
+)
+
+var number int
+
+func main() {
+	number = 710
+
+	fmt.Printf("Hex %X \nBinary %b \nDecimal %v\n", number, number, number)
+	number = number << 1
+	fmt.Print("Number was bitshifted << 1\n")
+	fmt.Printf("Hex %X \nBinary %b \nDecimal %v\n", number, number, number)
+	number = number >> 2
+	fmt.Print("Number was bitshifted >> 2\n")
+	fmt.Printf("Hex %X \nBinary %b \nDecimal %v\n", number, number, number)
+
+}
