9f7f0d2817b82850ef342a439e9df26edf0b2f49
joshuaparker <joshuanparker@gmail.com>
2020-06-17 20:58:32 -0400

    some refactoring.

--- bitshift2.go
+++ bitshift2.go
@@ -6,16 +6,17 @@ import (
 
 const (
 	_ = iota // _ tells Go I don't care about what returns here.
-	//kb := 1024
+
 	kb = 1 << (iota * 10)
-	//mb := 1024 * kb
 	mb = 1 << (iota * 10)
-	//gb := 1024 * mb
 	gb = 1 << (iota * 10)
 )
 
 func main() {
 
+	//kb := 1024
+	//mb := 1024 * kb
+	//gb := 1024 * mb
 	fmt.Printf("%d\t\t\t%b\n", kb, kb)
 	fmt.Printf("%d\t\t\t%b\n", mb, mb)
 	fmt.Printf("%d\t\t\t%b\n", gb, gb)
