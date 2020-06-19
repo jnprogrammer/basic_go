d789971d35e10d9ee6637ef7c148b075178cf335
joshuaparker <joshuanparker@gmail.com>
2020-06-18 23:55:34 -0400

    Using nested for loops in Go

--- controlflow2.go
+++ controlflow2.go
@@ -4,13 +4,12 @@ import "fmt"
 
 func main() {
 	i := 1
-	for i <= 3 {
-		fmt.Println(i)
-		i++
-	}
 	//   init ; condition; execute after 1 trip of the for loop
 	for x := 0; x <= 420; x++ {
 		fmt.Println(x)
-
+		for i <= 3 {
+			fmt.Println(i)
+			i++
+		}
 	}
 }
