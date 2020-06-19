95183dbe8601046bc1a19963bd7af227bd2b3373
joshuaparker <joshuanparker@gmail.com>
2020-06-18 23:58:37 -0400

    Testing removing the post action from the forloop

--- controlflow2.go
+++ controlflow2.go
@@ -5,11 +5,13 @@ import "fmt"
 func main() {
 	i := 1
 	//   init ; condition; execute after 1 trip of the for loop
-	for x := 0; x <= 420; x++ {
+	for x := 0; x <= 420; {
 		fmt.Println(x)
 		for i <= 3 {
-			fmt.Println(i)
+			fmt.Println("THIS IS I", i)
 			i++
+
 		}
+		x++
 	}
 }
