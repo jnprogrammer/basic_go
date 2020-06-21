cf6f53c7b6b98c4de9a4c2405fd34afdf98ec9fc
jnprogrammer <joshuanparker@gmail.com>
2020-06-20 23:59:58 -0400

    Understanding how Go handles true false

---
+++ controlflow5.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//true false are predeclared constances.
+	if true {
+		fmt.Println("001")
+	}
+	if false {
+		fmt.Println("002")
+
+	}
+	if !true {
+		fmt.Println("003")
+	}
+	if !false {
+		fmt.Println("004")
+
+	}
+
+}
