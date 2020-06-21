2b8670e4f702aca28337315a946d430ca0967de3
jnprogrammer <joshuanparker@gmail.com>
2020-06-21 19:50:57 -0400

    Learning how Go uses switch cases, fallthrough isn't default behavior like other languages, it has to be specified with the keyword fallthrough.

---
+++ controlflow7.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+	//for i := 0; i < 420; i++ {
+	//	if i % 2 != 0 {
+	//		fmt.Println(i)
+	//	}
+	//}
+
+	//case statements in go don't have fallthrough in switch statements less specified
+
+	switch {
+	case false:
+		fmt.Print("Won't print")
+	case (2 == 4):
+		fmt.Println("also won't print")
+	case true:
+		fmt.Println("PRints")
+		fallthrough
+	case false:
+		fmt.Print("Prints anyway because of fallthrough in previous case\n")
+		//fallthrough
+	case true:
+		fmt.Print("True so it prints only if there is a fallthrough in the previous step")
+
+	}
+}
