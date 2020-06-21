85972b48d38c29259674ceac48e5fbd360f65a2f
jnprogrammer <joshuanparker@gmail.com>
2020-06-21 17:26:01 -0400

    using else if in Go

---
+++ controlflow6.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 420
+	x = 710
+	if x == 420 {
+		fmt.Println("Have a Groovy time")
+	} else if x == 710 {
+		fmt.Println("Oil time !")
+	} else {
+		fmt.Println("End the Drug War !!")
+	}
+}
