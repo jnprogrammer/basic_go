002b5cfb09f1ca0aa0eebe8bae79252e3232f9e2
jnprogrammer <joshuanparker@gmail.com>
2020-06-21 22:24:53 -0400

    Created a Slice using a composite literal. allows you to store values of the same type in one object.

---
+++ slices.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	//COMPOSITE LITERAL y := []type{values}
+	x := []int{4, 5, 6, 7, 420}
+	fmt.Println(x)
+	// a SLICE allows you to group together VALUES of the same TYPE
+}
