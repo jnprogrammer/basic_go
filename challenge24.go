a2c80654cef64e501d57258d51637c1d354edcc6
joshuaparker <joshuanparker@gmail.com>
2020-06-23 00:06:29 -0400

    I added how to delete from a map to the same challenge.

--- challenge24.go
+++ challenge24.go
@@ -11,14 +11,28 @@ func main() {
 		"Fire":  3,
 		"Air":   4,
 	}
+	fmt.Println("****************")
 
 	for key, val := range m {
 		fmt.Println(key, val)
 	}
-
+	fmt.Println("****************")
 	// adding a record
 
 	m["Avatar"] = 5
+	m["Zuko"] = 420
+
+	for key, val := range m {
+		fmt.Println(key, val)
+	}
+	fmt.Println("****************")
+
+	//if v, ok := m["Zuko"]; ok {
+	//	delete(m, "Zuko")
+	//}
+	fmt.Println("Delete Zuko")
+	delete(m, "Zuko")
+	fmt.Println("****************")
 
 	for key, val := range m {
 		fmt.Println(key, val)
