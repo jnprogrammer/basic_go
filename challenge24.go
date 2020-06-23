23d053d9748da51cd8d81fdd13fa12d79f4fbae9
joshuaparker <joshuanparker@gmail.com>
2020-06-22 23:56:47 -0400

    Completed challenge where I create a map and printed out its key value pair with its index.

---
+++ challenge24.go
@@ -0,0 +1,18 @@
+// create a map with key TYPE string, store three records in the map, print out all values with their index too.
+
+package main
+
+import "fmt"
+
+func main() {
+	m := map[string]int{
+		"Water": 1,
+		"Earth": 2,
+		"Fire":  3,
+		"Air":   4,
+	}
+
+	for key, val := range m {
+		fmt.Println(key, val)
+	}
+}
