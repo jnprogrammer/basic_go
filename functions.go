998d0b5b675cc5fca12e550a99bf77e23917d3cd
jnprogrammer <joshuanparker@gmail.com>
2020-06-24 23:51:03 -0400

    Learned how functions return values in Go

--- functions.go
+++ functions.go
@@ -5,6 +5,13 @@ import "fmt"
 func main() {
 	foo()
 	bar("Tera)")
+	wrds := retro("420")
+	fmt.Println(wrds)
+
+	x, y := dude("A ", "Dude")
+
+	fmt.Println(x)
+	fmt.Println(y)
 }
 
 // func (r receiver) identifier(paramerters) (returns(s)) {  ...  }
@@ -17,4 +24,15 @@ func bar(s string) {
 	fmt.Println("Hey ", s)
 }
 
+func retro(s string) string {
+	return fmt.Sprint("Hello hey im printing to a string with sPrint or string print ", s)
+}
+
 // Everything in Go is PAss by Value
+
+func dude(fn string, ln string) (string, bool) {
+	a := fmt.Sprint(fn, ln, `,says hey`)
+
+	b := false
+	return a, b
+}
