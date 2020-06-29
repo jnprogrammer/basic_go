0f5a95b69444010a4e0a33d6efff7c60be42e92f
jnprogrammer <joshuanparker@gmail.com>
2020-06-28 22:53:10 -0400

    Fixed a type in the names, I skipped the number 5.

---
+++ functions6.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"fmt"
+)
+
+type electricvechical struct {
+	motors              int
+	batterycap          int
+	manufacturer        string
+	model               string
+	chargenetworkaccess bool
+	drivetrain          string
+	topspeedkm          int
+}
+
+type electrictruck struct {
+	electricvechical
+	towcap          int
+	groundclearance int
+}
+
+type ecars interface {
+	honk()
+}
+
+func main() {
+	ctruck := electrictruck{
+		electricvechical: electricvechical{
+			motors:              3,
+			batterycap:          806,
+			manufacturer:        "Tesla",
+			model:               "CyberTruck",
+			chargenetworkaccess: true,
+			drivetrain:          "Tri Motor All-Wheel Drive",
+			topspeedkm:          120,
+		},
+		towcap:          14000,
+		groundclearance: 16,
+	}
+	atruck := electrictruck{
+		electricvechical: electricvechical{
+			motors:              1,
+			batterycap:          450,
+			manufacturer:        "Tesla",
+			model:               "CyberTruck",
+			chargenetworkaccess: true,
+			drivetrain:          "Single Motor Rear-Wheel Drive",
+			topspeedkm:          120,
+		},
+		towcap:          7000,
+		groundclearance: 16,
+	}
+	ctruck.honk()
+
+	c := ecars(ctruck)
+	a := ecars(atruck)
+
+	fmt.Println(a, c)
+}
+
+func (s electrictruck) honk() {
+	fmt.Println("This is where I'm from and what made me: ", s.manufacturer, s.model, s.drivetrain)
+}
+
+// A VALUE can be of more than one TYPE
+
+func show(e ecars) {
+	fmt.Println("I was passed to show", e)
+}
