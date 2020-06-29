a7e0553e39ca9e6653395891d3ae3a0f1f9ffee4
jnprogrammer <joshuanparker@gmail.com>
2020-06-28 23:09:59 -0400

    Completed a challenge where I made two structs then made a method for both of those structs called area, then i made an interface called shape for any value that has an area() method.

---
+++ challenge33.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+type shape interface {
+	area()
+}
+
+type circle struct {
+	r  float64
+	pi float64
+}
+
+type square struct {
+	l int
+	w int
+}
+
+func main() {
+	circle64 := circle{
+		r:  42.2,
+		pi: 3.14159265359,
+	}
+
+	square94 := square{
+		l: 43,
+		w: 89,
+	}
+
+	shape.area(circle64)
+	shape.area(square94)
+}
+
+func (c circle) area() {
+	a := c.pi * c.r
+	fmt.Println("The Area of this circule is : ", a)
+}
+
+func (s square) area() {
+	a := s.l * s.w
+	fmt.Print("The Area of the Square is: ", a)
+}
