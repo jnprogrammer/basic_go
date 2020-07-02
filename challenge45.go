d9ef00e8ae909e30c02c98e7c4dd92ead59f04a7
joshuaparker <joshuanparker@gmail.com>
2020-07-02 00:00:01 -0400

    I completed a challenge where I used a custom sort

---
+++ challenge45.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+type Carrot struct {
+	Lenghcm float32
+	Weightg float32
+	Strain  string
+	Color   string
+}
+
+type ByCarrot []Carrot
+
+func (c ByCarrot) Len() int           { return len(c) }
+func (c ByCarrot) Swap(i, j int)      { c[i], c[j] = c[j], c[i] }
+func (c ByCarrot) Less(i, j int) bool { return c[i].Weightg > c[j].Weightg }
+
+func main() {
+	redcarrots := Carrot{
+		Strain:  "Firebrand",
+		Color:   "Neon Red",
+		Weightg: 4.32,
+		Lenghcm: 2.23,
+	}
+
+	purplecarrots := Carrot{
+		Strain:  "soothingpurps",
+		Color:   "Dark Purple",
+		Weightg: 6.12,
+		Lenghcm: 3.40,
+	}
+
+	greencarrots := Carrot{
+		Strain:  "druidsbroom",
+		Color:   "Bright Green",
+		Weightg: 3.86,
+		Lenghcm: 5.237,
+	}
+
+	pinkcarrots := Carrot{
+		Strain:  "candyclaw",
+		Color:   "Neon Pink",
+		Weightg: 8.964,
+		Lenghcm: 5.22,
+	}
+
+	patch := []Carrot{redcarrots, purplecarrots, greencarrots, pinkcarrots}
+	fmt.Println(patch)
+
+	sort.Sort(ByCarrot(patch))
+	fmt.Println(patch)
+
+}
+
+func (c Carrot) String() string {
+	return fmt.Sprintf("Carrot: %s  color: %s  weight: %vg length: %vkm \n", c.Strain, c.Color, c.Weightg, c.Lenghcm)
+}
