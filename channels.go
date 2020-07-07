9dbec58e10ce0c3fb1e79fc281be64bef282b650
joshuaparker <joshuanparker@gmail.com>
2020-07-06 20:43:13 -0400

    Learned about Channels in go. Channels in block. You must have send / receive meet at the same time or you'll have a deadlock.

---
+++ channels.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	c := make(chan int, 100)
+
+	//go func(){
+	//	c <- 420
+	//}()
+	//c <-710
+	//c <-7168
+	//c <-7186
+	//c <-7780
+	//c <-760
+	//c <-720
+	//c <-730
+	//c <-740
+
+	fmt.Println(<-c, <-c)
+}
+
+// Go channels work by a receive/send lock by channels, without this you'll get a deadlock
