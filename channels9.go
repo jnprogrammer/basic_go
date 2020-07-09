536ba1d2901583d08f55847da8ec5dedc28ed629
joshuaparker <joshuanparker@gmail.com>
2020-07-09 11:50:11 -0400

    Reviewd the fanout and the fan in design patters and that you can throttle the amount of goroutines you make. const goroutines = 10 , you use that and launch goroutines equal to that number.

--- channels9.go
+++ channels9.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"sync"
+	"time"
 )
 
 func main() {
@@ -10,5 +13,36 @@ func main() {
 
 	go populate(c1)
 
-	go fanIn(c1, c2)
+	go faninout(c1, c2)
+
+	for v := range c2 {
+		fmt.Println(v)
+	}
+
+	fmt.Println("I'm gonna exit")
+}
+
+func populate(c chan int) {
+	for i := 0; i < 100; i++ {
+		c <- i
+	}
+	close(c)
+}
+
+func faninout(c1, c2 chan int) {
+	var waitinggroup sync.WaitGroup
+	for v := range c1 {
+		waitinggroup.Add(1)
+		go func(v2 int) {
+			c2 <- timeConsumingWork(v2)
+			waitinggroup.Done()
+		}(v)
+	}
+	waitinggroup.Wait()
+	close(c2)
+}
+
+func timeConsumingWork(n int) int {
+	time.Sleep(time.Microsecond * time.Duration(rand.Intn(500)))
+	return n + rand.Intn(1000)
 }
