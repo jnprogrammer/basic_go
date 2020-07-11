a28003e3791fbbd137d6667385d5a647e7275571
jnprogrammer <joshuanparker@gmail.com>
2020-07-10 23:55:16 -0400

    Completed a challenge on fixing a channel by making it bidirectional. And a 2nd one for good measure.

---
+++ challenge55.go
@@ -0,0 +1,36 @@
+//package main
+//
+//import (
+//	"fmt"
+//)
+//
+//func main() {
+//	cr := make(<-chan int)
+//
+//	go func() {
+//		cr <- 42
+//	}()
+//	fmt.Println(<-cr)
+//
+//	fmt.Printf("------\n")
+//	fmt.Printf("cr\t%T\n", cr)
+//}
+// Fix code above
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	cr := make(chan int)
+
+	go func() {
+		cr <- 42
+	}()
+	fmt.Println(<-cr)
+
+	fmt.Printf("------\n")
+	fmt.Printf("cr\t%T\n", cr)
+}
