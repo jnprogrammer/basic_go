2cd371df6122ae8bf4f599ee01eb3ada23efd6bf
joshuaparker <joshuanparker@gmail.com>
2020-07-15 11:32:46 -0400

    Still learning about proper documentation in go

--- challenge65.go
+++ challenge65.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"fmt"
 	"testing"
-
 )
 
 type testpair struct {
--- godocs.go
+++ godocs.go
@@ -9,3 +9,13 @@ func Sum(x . . .int) int {
 	}
 	return  sum
 }
+
+/*
+So the coolest thing about go when it comes to documentation
+is that when you create comments and upload your code to github, you can
+have that code searchable in godocs, and go docs will display them formatted
+
+
+Remember when making documentation you start with the name of what your
+documenting first and write in complete sentences.
+ */
