b0914334f30b713134594923f6df000c2d9a2244
joshuaparker <joshuanparker@gmail.com>
2020-07-16 08:08:56 -0400

    I've learned about func init() and how its useful when writing go code.

--- learnpackages/main.go
+++ learnpackages/main.go
@@ -3,18 +3,31 @@ package main
 import (
 	"fmt"
 	"learnpackages/simpleinterest"
+	"log"
 )
 
-func main() {
+var p, r, t = 75000.0, 24.5, 10.0
 
+func init() {
 	fmt.Println(" Simple interest calculator")
 	fmt.Println(" Retyped from Blog")
 	fmt.Println("https://golangbot.com/go-packages/")
 	fmt.Println("To re-enforce learning go packages")
 
-	p := 5000.0
-	r := 10.0
-	t := 15.0
+	println("main package initlize")
+	if p < 0 {
+		log.Fatal("Principal is less than zero")
+	}
+	if r < 0 {
+		log.Fatal("Rate of interest is less than zero")
+	}
+	if t < 0 {
+		log.Fatal("Duration is less than zero")
+	}
+
+}
+
+func main() {
 
 	si := simpleinterest.Calculate(p, r, t)
 	fmt.Println("Simple interest is", si)
