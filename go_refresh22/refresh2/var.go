34656c33c2d1f9f40b517f872ea75067ac6766db
jnprogrammer <joshuanparker@gmail.com>
2022-09-24 19:44:23 -0400

    variable refresh

--- go_refresh22/refresh2/var.go
+++ go_refresh22/refresh2/var.go
@@ -1,12 +1,26 @@
 package main
 
+import (
+	"fmt"
+	"reflect"
+	"strconv"
+)
+
 var (
-	name   string
-	course string
-	module int
-	clip   int
+	name, course = "Josh", "Master of This.exe"
+	module       = "4"
+	clip         = 710 // just a line showing an example.
 )
 
 func main() {
+	fmt.Println("Name and course are set to ", name, " and ", course, ".")
+	fmt.Println("Module and clip are set to ", module, "and", clip, ".")
+	fmt.Println("Name is of type", reflect.TypeOf(name))
+	fmt.Println("Module is of type", reflect.TypeOf(module))
 
+	iModule, err := strconv.Atoi(module)
+	if err == nil {
+		total := iModule + clip
+		fmt.Println("Module plus clip equals", total)
+	}
 }
