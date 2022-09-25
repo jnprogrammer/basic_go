0064fcb2b2e1d7858cc6177b9c9036eecd4692c0
jnprogrammer <joshuanparker@gmail.com>
2022-09-25 15:41:58 -0400

    looking at how constances work in go

---
+++ go_refresh22/refresh6/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	const c = 186000
+	fmt.Println("the speed of light is", c, "miles per second")
+
+	c = 420
+	fmt.Println("the speed of light is", c, "miles per second")
+
+}
