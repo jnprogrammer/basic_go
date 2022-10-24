ba3d11c1022769f4155c04004d1e0a9ad39b910a
j <joshuanparker@gmail.com>
2022-10-23 22:01:05 -0400

    inline slice

--- go_refresh22/refresh16/main.go
+++ go_refresh22/refresh16/main.go
@@ -6,10 +6,7 @@ import (
 
 func main() {
 	//slices don't store data, they point to an array
-	course := make([]string, 5, 10)
-	course[0] = "Reading books"
-	course[1] = "Baking books"
-	course[2] = "Eating books"
+	course := []string{"Reading books", "Baking books", "Eating books"}
 
 	fmt.Printf("Length of slice is %d and capacity is %d\n",
 		len(course), cap(course))
