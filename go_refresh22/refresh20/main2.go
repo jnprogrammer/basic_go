b8b48383061cc4351b29dee69ffc5576b1a52367
jnprogrammer <joshuanparker@gmail.com>
2022-10-25 18:44:30 -0400

    Concurrency and waitgroups

--- go_refresh22/refresh20/main2.go
+++ go_refresh22/refresh20/main2.go
@@ -2,17 +2,24 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
 func main() {
 
+	var waitGrp sync.WaitGroup
+	waitGrp.Add(2)
+
 	func() {
+		defer waitGrp.Done()
 		time.Sleep(5 * time.Second)
 		fmt.Println("Hello Go for the 3220 time\nI KEEP FIGHTING TO CHANGE AND I KEEP FIGHTING")
 	}()
 
 	func() {
+		defer waitGrp.Done()
 		fmt.Println("Cats are not food")
 	}()
+	waitGrp.Wait()
 }
