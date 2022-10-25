963549fd8e0aeec6abb9a530757b3431363fe0f0
jnprogrammer <joshuanparker@gmail.com>
2022-10-24 21:33:27 -0400

    Taking a slice of a slice

--- go_refresh22/refresh17/main.go
+++ go_refresh22/refresh17/main.go
@@ -6,7 +6,15 @@ import (
 
 func main() {
 
-	mySlice := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	mySlice := []int{1, 2, 3, 909, 710, 6, 7, 8, 9, 10}
 
 	fmt.Println(mySlice[3])
+
+	mySlice[1] = 420
+	fmt.Println(mySlice)
+
+	//slice a slice is gotastic
+	//
+	sliceOfSlice := mySlice[3:]
+	fmt.Println(sliceOfSlice)
 }
